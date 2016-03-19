b93d0dfa33b30e01a22d42d3ae08c22c18d04cff
pathapanda <pathanachansamone@gmail.com>
2016-03-19 08:58:17 +0100

    playermoves

---
+++ godot/scripts/player.gd
@@ -0,0 +1,43 @@
+
+extends Sprite
+
+export var speed = 50.0
+var heading_direction_h = 0.0
+var heading_direction_v = 0.0
+var anim
+
+func _ready():
+	set_process(speed)
+	anim = get_node("./animator")
+
+func _process(deltatime):
+	var pos = get_pos()
+	pos.x += speed * deltatime * heading_direction_h
+	pos.y += speed * deltatime * heading_direction_v
+	pos = bound_pos(pos)
+	set_pos(pos)
+	
+func bound_pos(pos):
+	if pos.x > 152:
+		pos.x = 152
+	if pos.x < 28:
+		pos.x = 28
+	return pos
+	
+func go_left():
+	heading_direction_h = -1.0
+	
+func go_right():
+	heading_direction_h = 1.0
+	
+func go_up():
+	heading_direction_v = -1.0
+
+func go_down():
+	heading_direction_v = 1.0
+
+func stop_h():
+	heading_direction_h = 0.0
+	
+func stop_v():
+	heading_direction_v = 0.0
--- godot/scripts/playercontroller.gd
+++ godot/scripts/playercontroller.gd
@@ -1,22 +1,54 @@
 
 extends Node
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
+var player
+var last_h_axis = 0
+var last_w_axis = 0
 
 func _ready():
+	player = get_node("..")
 	set_process(true)
 	
 func _process (deltatime):
-	if Input.is_key_pressed(KEY_LEFT):
-		print ("A gauche")
-	if Input.is_key_pressed(KEY_RIGHT):
-		print ("A droite")
-	if Input.is_key_pressed(KEY_UP):
-		print ("En haut")
-	if Input.is_key_pressed(KEY_DOWN):
-		print ("En bas")
+	var h_axis = get_h_axis()
+	var w_axis = get_w_axis()
+	
+	if last_h_axis != h_axis or last_w_axis != w_axis:
+		move_player(h_axis, w_axis)
+	
+	last_h_axis = h_axis
+	
+	last_w_axis = w_axis
+	
+func get_h_axis():
+	var h_axis = 0
+	if Input.is_action_pressed("ui_left"):
+		h_axis -= 1
+	if Input.is_action_pressed("ui_right"):
+		h_axis += 1
+	return h_axis
+	
+func get_w_axis():
+	var w_axis = 0
+	if Input.is_action_pressed("ui_up"):
+		w_axis += 1
+	if Input.is_action_pressed("ui_down"):
+		w_axis -= 1
+	return w_axis
+	
+func move_player(h_axis, w_axis):
+	if h_axis > 0:
+		player.go_right()
+	elif h_axis < 0:
+		player.go_left()
+	else:
+		player.stop_h()
+	if w_axis > 0:
+		player.go_up()
+	elif w_axis < 0: 
+		player.go_down()
+	else:
+		player.stop_v()
 	
 	
 

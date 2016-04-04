245c7a00337e928af41950c4b5b86bd877eb706e
pathapanda <pathanachansamone@gmail.com>
2016-04-04 16:10:13 +0200

    timer == 0, game over == true

--- godot/scripts/timer.gd
+++ godot/scripts/timer.gd
@@ -1,11 +1,13 @@
 extends RichTextLabel
 
-var count = 180
+var count = 3
 
 func _ready():
-	self.add_text("180")
+	self.add_text("3")
 
 func _on_Timer_timeout():
 	count -= 1
 	self.clear()
 	self.add_text(str(count))
+	if count < 0:
+		get_tree().change_scene("res://scenes/game_over.scn")

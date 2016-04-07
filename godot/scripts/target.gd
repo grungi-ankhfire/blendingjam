dbd4e4b16fd22b2077a8b22756e34a50dbca7bca
Bastien Gorissen <kadomony@gmail.com>
2016-04-07 13:05:18 +0200

    Started improving the AI

--- godot/scripts/target.gd
+++ godot/scripts/target.gd
@@ -4,11 +4,13 @@ extends Node2D
 # member variables here, example:
 # var a=2
 # var b="textvar"
-
+export(NodePath) var bed
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
-
+	
+func get_bed():
+	return get_node(bed)
 

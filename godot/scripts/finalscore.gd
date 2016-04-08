7493f8cb8e5fdbce04c1eac71aa39da3be8efce6
Gorissen Bastien <Gorissen Bastien>
2016-04-09 01:38:20 +0200

    High score

--- godot/scripts/finalscore.gd
+++ godot/scripts/finalscore.gd
@@ -6,10 +6,19 @@ extends Label
 # var b="textvar"
 
 var score = 0
+var high_score = 0
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	score = Globals.get("SCORE") + 5 * Globals.get("TIMER")
-	self.text = "Your score : " + str(score)
+	score = 0
+	high_score = 0
+	if Globals.has("SCORE") and Globals.has("TIMER"):
+		score = Globals.get("SCORE") + 5 * Globals.get("TIMER")
+	if Globals.has("HIGH_SCORE"):
+		high_score = Globals.get("HIGH_SCORE")
+	if score > high_score:
+		Globals.set("HIGH_SCORE", score)
+		high_score = score
+	self.text = "Your score : " + str(score) + "\nHigh score : " + str(high_score)
 
